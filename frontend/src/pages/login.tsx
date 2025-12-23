a61e61a8c0bd2dc93496e4d8e8e680498efa3a28
sorin <kimsolin061213@gmail.com>
2025-12-23 18:41:43 +0900

    feat: 로그인 기능 구현

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { LoginForm } from '../features/auth/LoginForm';
+
+import { Header } from '../shared/layout/Header';
+
+import { Footer } from '../shared/layout/Footer';
+
+export default function **LoginPage**() {
+
+return (
+
+<>
+
+<Header />
+
+<main>
+
+<LoginForm />
+
+</main>
+
+<Footer />
+
+</>
+
+);
+
+}
